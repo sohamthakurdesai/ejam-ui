4bee2391c35d67d889efd7e47201aa6fd116ef51
sohamthakurdesai <sohamt@valueaddsofttech.com>
2020-06-30 20:21:22 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './App.css';
 import { Container } from 'reactstrap';
 import { useDispatch } from 'react-redux';
-import TemplateForm from './components/TemplateForm';
+import TemplateForm from './Components/TemplateForm';
 import { getTemplatesAndVersions, getDeployments } from './actions/applicationActions'
-import CountdownTimer from './components/CountdownTimer';
+import CountdownTimer from './Components/CountdownTimer';
 
 
 function App() {
